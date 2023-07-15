5e46751cc37d4a159f9dc471d99b025c1aec68a6
pengSherryYel <peng_sherry@outlook.com>
2023-07-16 01:28:05 +0200

    add pre_env

---
+++ prepare_env.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+mkdir resources
+cd env
+echo "prepare phact"
+conda env create -n RP_phacts --file ./RP_phacts.yml
+
+echo "prepare bacphlip"
+conda env create -n RP_bacphlip --file ./RP_bacphlip.yml
+
+echo "prepare deephage"
+conda env create -n RP_deephage --file RP_Deephage.yml
+sh RP_Deephage.extra.sh
+
+echo "prepare phabox"
+conda env create -n RP_phabox --file RP_phabox.yml
+sh RP_phabox.extra.sh
+
+echo 'prepare replidec'
+conda create -n RP_replidec -c bioconda replidec
+
+echo 'prepare phageAI'
+conda env create -n RP_phageai --file RP_phageai.yml
+
+echo 'YEAH!! Enviroment prepare done!!'
+
+
